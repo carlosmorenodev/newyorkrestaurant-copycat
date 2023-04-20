4d7d88b4167cd628c68dcd8f978f44e5ad89a1a1
carlosmorenodev <carlosmorenomartindev@gmail.com>
2023-04-20 11:09:23 +0200

    Primer commit

---
+++ main.js
@@ -0,0 +1,9 @@
+let responsiveMenu = document.getElementById("responsiveMenu");
+let mainMenu = document.getElementById("mainMenu");
+
+
+responsiveMenu.addEventListener("click", () => {
+    mainMenu.classList.toggle('main-menu-active');
+
+})
+
